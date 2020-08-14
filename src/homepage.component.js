07c330dfa9ebf9a3b51ca026ce2e1810619474f7
Makankan <matadorsmca@gmail.com>
2020-08-14 11:33:07 +0530

    inital commit

---
+++ src/homepage.component.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Mobiles</h1>
+            <span className="subtitle">ShopNow</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Tablets</h1>
+            <span className="subtitle">ShopNow</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Laptops</h1>
+            <span className="subtitle">ShopNow</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Accesories</h1>
+            <span className="subtitle">ShopNow</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Gadgeds</h1>
+            <span className="subtitle">ShopNow</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
